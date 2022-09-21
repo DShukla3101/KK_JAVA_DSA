94cf2f2e77286a7e491c0ce3818b77348b8656b3
Dheeraj <ds077099@gmail.com>
2022-09-21 15:43:31 +0530

    A fuction to find the greater Number between two Numbers.

---
+++ L2_first_java/GreaterNum.java
@@ -0,0 +1,27 @@
+package L2_first_java;
+import java.util.Scanner;
+
+public class GreaterNum{
+
+    public static int getGreater(int num1, int num2){
+        if(num1>num2){
+            return num1;
+        }
+        else return num2;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter Second Number : ");
+        int num2 = sc.nextInt();
+
+        int greaterNumber = getGreater(num1, num2);
+        System.out.println(greaterNumber+" is Greater.");
+        sc.close();
+        
+    }
+
+}
